dd483b2d89643a48388b6b38ef03e8ae8746392e
Ron Jacob <generalsmoke23@gmail.com>
2022-07-11 17:33:38 +0530

    Web Scraping Completed

--- scraper/code-scraper.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-with open('simple.html') as html_file:
-  soup = BeautifulSoup(html_file,'lxml')
--- scraper/codeforces_id_to_sub.py
+++ scraper/codeforces_id_to_sub.py
@@ -2,45 +2,51 @@ import requests, bs4, csv
 
 
 
-with open('../codeforces/problemset_completed.csv','r',encoding = 'utf-8',newline = '') as f:
-    readFile = csv.reader(f)
-    
-    for row in readFile:
-        PROBLEM_NUMBER = '282'
-        PROBLEM_LETTER = 'A'
 
-        categories = ['CE','OK','OTHR','WA']
 
-        for cat in categories:
-            i = 0
+categories = ['OK','CE','OTHR','WA']
 
-            with open('codeforces/{}{}_GNUC/submission_ids_{}_{}{}GNUC.csv'.format(PROBLEM_NUMBER,PROBLEM_LETTER,cat,PROBLEM_NUMBER,PROBLEM_LETTER),'r',encoding='utf-8', newline='') as r:
-                
-                reader = csv.reader(r)
-                
-                with open('codeforces/{}{}_GNUC/submissions_{}_{}{}GNUC.csv'.format(PROBLEM_NUMBER,PROBLEM_LETTER,cat,PROBLEM_NUMBER,PROBLEM_LETTER),'w',encoding='utf-8', newline = '') as w:
+for cat in categories:
+    i = 0
 
-                    writer = csv.writer(w)
-                    
-                    for row in reader:
-                        
-                        res = requests.get('http://codeforces.com/contest/{}/submission/{}'.format(PROBLEM_NUMBER,row[0]))
+    with open('../codeforces/submission_ids_{}.csv'.format(cat),'r',encoding='utf-8', newline='') as r:
+        
+        reader = csv.reader(r)
+        
+        with open('../codeforces/submissions/code_snippets_{}.csv'.format(cat),'w',encoding='utf-8', newline = '') as w:
 
-                        #check if the download succeeded 
-                        try:
-                            res.raise_for_status()
-                        except Exception as exc:
-                            print('There was a problem: {}'.format(exc))
-                
-                        #turn the context (res.text) into a BeautifulSoup object
-                        site_content = bs4.BeautifulSoup(res.text,"html.parser")
+            writer = csv.writer(w)
             
-                        #actual submission inside denoted by the <pre class="prettyprint program-source".
-                        elems = site_content.find_all("pre", { "class" : "prettyprint lang-c program-source" })
+            for row in reader:
+                print(row)
+                
+                PROBLEM_NUMBER = row[0]
+                PROBLEM_LETTER = row[1]
+                PROBLEM_NAME = row[2]
+                SUBMISSION_ID = row[3]
+                VERDICT = row[4]
+                TIME = row[5]
+                MEMORY = row[6]
+                
+                
+                res = requests.get('http://codeforces.com/contest/{}/submission/{}'.format(PROBLEM_NUMBER,SUBMISSION_ID))
 
-                        print('got {} submission: {}'.format(cat,i))
-                        i += 1
-                        
-                        writer.writerow(['<START> {} <END>'.format(elems[0].getText()),row[1]])
-        print('DONE!')
+                #check if the download succeeded 
+                try:
+                    res.raise_for_status()
+                except Exception as exc:
+                    print('There was a problem: {}'.format(exc))
+        
+                #turn the context (res.text) into a BeautifulSoup object
+                site_content = bs4.BeautifulSoup(res.text,"html.parser")
+    
+                #actual submission inside denoted by the <pre class="prettyprint program-source".
+                elems = site_content.find_all("pre", { "id" : "program-source-text" })
+                print(elems)
+
+                print('got {} submission: {} problem number: {} problem_letter: {} submission_id: {}'.format(cat,i,PROBLEM_NUMBER,PROBLEM_LETTER,SUBMISSION_ID))
+                i += 1
+                
+                writer.writerow(['<START> {} <END>'.format(elems[0].getText()),VERDICT,TIME,MEMORY])
+print('DONE!')
     
