157996760d21f07d0f2a1b9efef0c9026ec53c0f
Ron Jacob <generalsmoke23@gmail.com>
2022-09-15 19:35:22 +0530

    Errors Class part1 Complete

--- mining/inducers/error_inducer.py
+++ mining/inducers/error_inducer.py
@@ -1,166 +1,253 @@
 import code
 import re
+import shutil
+import os
 
-    
 
 class errors:
 
-    def fileOpen(self,filename):
-        with open (filename,'r',encoding='utf-8') as f:
-            self.codeSnip = f.readlines()
+    def fileOpen(self, filename):
+        if os.path.exists('../../test_codes/tests'):
+            shutil.rmtree('../../test_codes/tests')
+        os.mkdir('../../test_codes/tests')
+        with open(filename, 'r', encoding='utf-8') as f:
+            self.codeSnip1 = f.read()
             f.close()
-            
-    def __fileWrite__(self, errorSnip, error,no):
-        with open(f'../../test_codes/E_test_{error}_{no}',"w",encoding='utf-8') as f:
+        with open(filename, 'r', encoding='utf-8') as f:
+            self.codeSnip2 = f.readlines()
+            f.close()
+
+        self.__detectOperators(self.codeSnip1)
+        self.__searchKeywords(self.codeSnip1)
+
+    def __fileWrite(self, errorSnip, error, no):
+        with open(f'../../test_codes/tests/E_test_{error}_{no}', "w", encoding='utf-8') as f:
             f.write("".join(errorSnip))
             f.close()
-            
 
+    def __detectOperators(self, codeSnip):
+        self.__operatorsList = []
+        searchOperators = re.finditer('(==|<=|>=|!=|\*=|\/=|\+=|-=)', codeSnip)
+        for pos in searchOperators:
+            startPos = pos.start()
+            endPos = pos.end()
+            self.__operatorsList.append((startPos, endPos))
+        # print(self.__operatorsList)
+        
+    def __searchKeywords(self, codeSnip):
+        self.__keywordsList = []
+        searchDatatype = re.finditer(
+            '(int|bool|float|double|char\b)\s', codeSnip)
+        # print(list(searchDatatype))
+        for i in searchDatatype:
+            end = i.end()
+            l = 0
+            while codeSnip[end] not in "(;":
+                if codeSnip[end] == '{':
+                    l = 1
+                    break
+                end += 1
+            if l:
+                continue
+            self.__keywordsList.append((i.end(), end))
 
-    def emptyLoop(self, perFileError = 10, noOfErr = 20):
-        itr = 0;filePtr = 0
+    def emptyLoop(self, perFileError=10, noOfErr=20):
+        itr = 0
+        filePtr = 0
         n = len(self.codeSnip)
-        while noOfErr and filePtr != n: 
+        while noOfErr and filePtr != n:
             code = self.codeSnip.copy()
-            a=["while","for"]
+            a = ["while", "for"]
             edit = 0
             tempFileError = perFileError
-            for i in range(filePtr,len(code)):
+
+            for i in range(filePtr, len(code)):
                 filePtr = i+1
                 # if "while" in code[i]:
                 if any(x in code[i] for x in a):
-                    edit = 1    
+                    edit = 1
                     tmp = list(code[i])
-                    
+
                     for j in range(len(tmp)):
-                        if tmp[j]==")":
-                            tmp=tmp[0:j+1]+[";"]+tmp[j+1:]
+                        if tmp[j] == ")":
+                            tmp = tmp[0:j+1]+[";"]+tmp[j+1:]
                             break
                     code.pop(i)
-                    code.insert(i,"".join(tmp))
+                    code.insert(i, "".join(tmp))
                     noOfErr -= 1
                     tempFileError -= 1
                 if not tempFileError:
-                    itr+=1
+                    itr += 1
                     break
             if edit:
-                self.__fileWrite__(code,"EmptyLoop",itr);
-            
-            
-    
-    def removeReturn(self,codeSnip,noOfErr = 2):
+                self.__fileWrite(code, "EmptyLoop", itr)
+
+    def removeReturn(self, codeSnip, noOfErr=2):
         codeSnip = codeSnip.readlines()
         pass
-    
 
-    def equalityComparisontoAssignment(self, perFileError = 10, noOfErr = 20):
-        itr = 0;filePtr = 0
-        n = len(self.codeSnip)
-        while noOfErr and filePtr != n:
-            for i in range(len(codesnip)):
-                if "if" in codesnip[i]:
-                    # tmp=list(codesnip[i])
-                    # index=codesnip.find("==")
-                    # print(index)
-                    tmp=codesnip[i].split()
-                    print(tmp)
-                    for j in range(len(tmp)):
-                        if "==" in tmp[j]:
-                            index=tmp[j].find("==")
-                            tmp[j]=tmp[j][:index]+tmp[j][index+1:]+'\n'        
-                    codesnip.pop(i)
-                    codesnip.insert(i," ".join(tmp))
-                    t-=1
-                if t==0:
-                    return codesnip
-            print("".join(codesnip))
-            return codesnip
-    
+    def equalityComparisonToAssignment(self, perFileError=10, noOfErr=20):
+        itr = 0
+        code = self.codeSnip1
+        tempFileErrors = perFileError
+        edit = 0
+        for start, end in self.__operatorsList:
+            if self.codeSnip1[start:end] != "==":
+                continue
+            startPos = start
+            while self.codeSnip1[startPos] not in ";":
+                startPos -= 1
+
+            endPos = end
+            while code[endPos] != ";":
+                endPos += 1
+
+            code = code.replace(
+                self.codeSnip1[startPos:endPos], f'{self.codeSnip1[startPos:end-1]}{self.codeSnip1[end:endPos]}')
+            tempFileErrors -= 1
+            noOfErr -= 1
+            edit = 1
+            if not tempFileErrors:
+                itr += 1
+                tempFileErrors = perFileError
+                self.__fileWrite(code, "compErr", itr)
+                code = self.codeSnip1
+                edit = 0
+            if not noOfErr:
+                itr += 1
+                self.__fileWrite(code, 'compErr', itr)
+                edit = 0
+                return
+        if edit:
+            itr += 1
+            self.__fileWrite(code, "compErr", itr)
+
     # [+=,-=,/=,%=]
-    def errors_in_assignment_operators(self,codesnip,t=2):
-        a=["while","for"]
-        c=["*=","/="]
-        for i in range(len(codesnip)):
-            if not any (x in codesnip[i] for x in a):
-                if "=" in codesnip[i] and not "==" in codesnip[i] and not "//" in codesnip[i]:
-                    print(i)
-                    print("the codesnip is",codesnip[i],)
-                    if any (y in codesnip[i] for y in c):
-                        tmpr=list(codesnip[i])
-                        index=tmpr.index("=")
-                        tmpr.insert(index+1,tmpr[index-1])
-                        tmpr.pop(index-1)
-                        print("".join(tmpr))
-                        codesnip.pop(i)
-                        codesnip.insert(i,"".join(tmpr))
-                        print(codesnip.index("".join(tmpr)))
-                        t-=1
-                    if t==0:
-                        return codesnip
-        print("".join(codesnip))
-        return codesnip
+    def errors_in_assignment_operators(self, perFileErrors, noOfErr=20):
+        itr = 0
+        code = self.codeSnip1
+        tempFileErrors = perFileErrors
+        edit = 0
+        for start, end in self.__operatorsList:
+            if self.codeSnip1[start:end] not in ["*=", "/="]:
+                continue
+            startPos = start
+            while self.codeSnip1[startPos] not in ";{":
+                startPos -= 1
+            endPos = end
+            while self.codeSnip1[endPos] != ";":
+                endPos += 1
+            code = code.replace(
+                self.codeSnip1[startPos:endPos], f"{self.codeSnip1[startPos:start]} {self.codeSnip1[end-1]}{self.codeSnip1[start]} {self.codeSnip1[end:endPos]} ")
+            tempFileErrors -= 1
+            noOfErr -= 1
+            edit = 1
+            if not tempFileErrors:
+                itr += 1
+                tempFileErrors = perFileErrors
+                self.__fileWrite(code, "assignErr", itr)
+                code = self.codeSnip1
+                edit = 0
+            if not noOfErr:
+                itr += 1
+                self.__fileWrite(code, "assignErr", itr)
+                edit = 0
+                return
+        if edit:
+            itr += 1
+            self.__fileWrite(code, "assignErr", itr)
 
-
-    def left_assignment_to_right(self,codesnip,t=2):
-        a=["while","for"]
-        b=["+","-","*","/"]
-        c=["+=","-=","*=","/="]
-        for i in range(len(codesnip)):
-            if not any (x in codesnip[i] for x in a):
-                if "=" in codesnip[i] and not "==" in codesnip[i] and not "//" in codesnip[i]:
-                    if not any (x in codesnip[i] for x in c):
-                        print(i)
-                        print("the codesnip is",codesnip[i],)
-                        if any(x in codesnip[i] for x in b):
-                            tmp=list(codesnip[i])
-                            index = tmp.index("=")
-                            index1 = tmp.index(";")
-                            tmp1=tmp[index+1:index1]
-                            tmp1.append("=")
-                            print(tmp1)
-                            for j in range(index):
-                                tmp1.append(tmp[j])
-                            tmp1.append(";\n")
-                            print(tmp1)
-                            tmp2=''.join(tmp1).replace(" ","")
-                            print(tmp2)
-                            codesnip.pop(i)
-                            codesnip.insert(i,tmp2)
-                            t-=1
-                        if t==0:
-                            return codesnip
-        print("".join(codesnip))
-        return codesnip
+    def left_assignment_to_right(self, perFileErrors, noOfErr = 20):
+        itr = 0
+        code =self.codeSnip1
+        tempFileErrors = perFileErrors
+        edit = 0
+        equalLoc = re.finditer("=", self.codeSnip1)
+        for pos in equalLoc:
+            l = 0
+            for start,end in self.__operatorsList:
+                if pos.start() >= start and pos.end() <= end:
+                    l = 1
+                    break
+            if l:
+                continue
+            startPos = pos.start()
+            l = 0
+            while self.codeSnip1[startPos] not in "{;":
+                startPos -= 1
+            startPos +=1
+            endPos = pos.end()
+            while self.codeSnip1[endPos] not in ";":
+                if self.codeSnip1[endPos] in "+" and self.codeSnip1[endPos+1] != "+":
+                    l = 1
+                if self.codeSnip1[endPos] in "-" and self.codeSnip1[endPos+1] != "-":
+                    l  = 1
+                if self.codeSnip1[endPos] in "*/":
+                    l = 1
+                endPos +=1    
+            if not l:
+                continue
+            code = code.replace(self.codeSnip1[startPos:endPos], f"{self.codeSnip1[pos.end():endPos]} = {self.codeSnip1[startPos:pos.start()]}")
+            tempFileErrors -=1
+            noOfErr -=1
+            edit = 1
+            if not tempFileErrors:
+                itr +=1
+                tempFileErrors = perFileErrors
+                self.__fileWrite(code,"invertAssign",itr)
+                code = self.codeSnip1
+                edit  = 0
+            if not noOfErr:
+                itr +=1
+                self.__fileWrite(code, "invertAssign", itr)
+                edit = 0
+                return
+        if edit:
+            itr +=1
+            self.__fileWrite(code, "inertAssign", itr)
             
 
-    def notypedeclaration(self,codesnip,t=2):
-            a=["int","float","double","char","wchar_t","bool","string"]
-            b=["main","for","="]
-            # for i in range(len(codesnip)):
-            #     if any(x in codesnip[i] for x in a):
-            #         if not any(x in codesnip[i] for x in b):
-            #             print(codesnip[i])
-            for i in range(len(codesnip)):
-                match=re.search("(int|char|bool|float|long|long Long|string)",codesnip[i])
-                if match!=None:
-                    startpos=match.start()
-                    endpos=match.end()
-                    codesnip[i]=codesnip[i][:startpos]+codesnip[i][endpos+1:]
-                    print(codesnip[i])
-                    t-=1
-                if t==0:
-                    return codesnip
-            return codesnip
-    
-    def else_if_concatenate(self,codesnip,t=2):
+    def notypedeclaration(self, perFileErrors, noOfErr = 20):
+        itr  = 0
+        code = self.codeSnip1
+        tempFileErrors = perFileErrors
+        edit = 0
+        for start, end in self.__keywordsList:
+            startPos = start
+            while self.codeSnip1[startPos] not in "{;":
+                startPos -=1
+            endPos = end
+            while self.codeSnip1[endPos] != ";":
+                endPos+=1
+            code = code.replace(self.codeSnip1[startPos:endPos],self.codeSnip1[start:endPos])
+            tempFileErrors -=1
+            noOfErr -=1
+            edit = 1
+            if not tempFileErrors:
+                itr +=1
+                tempFileErrors = perFileErrors
+                self.__fileWrite(code,"notDec",itr)
+                code = self.codeSnip1
+                edit = 0
+            if not noOfErr:
+                itr +=1
+                self.__fileWrite(code,"notDec",itr)
+                edit = 0
+                return 
+        if edit:
+            itr +=1
+            self.__fileWrite(code,"notDec",itr)
+            
+
+    def else_if_concatenate(self, codesnip, t=2):
         for i in range(len(codesnip)):
             if "else if" in codesnip[i]:
                 print(codesnip[i])
 
 
 Err = errors()
-codesnip=Err.fileOpen("../../test_codes/test_add.cpp")
-codesnip2=Err.emptyLoop(1)
+codesnip = Err.fileOpen("../../test_codes/test_add.cpp")
+Err.notypedeclaration(1)
 
 # codesnip=Err.fileopen()
 # codesnip2=Err.equalityComparisontoAssignment(codesnip)
--- mining/inducers/exception_inducer.py
+++ mining/inducers/exception_inducer.py
@@ -1,32 +1,30 @@
 import random
 import re
 
+
 class ExceptionInducer:
 
-    def fileOpen(self,fileName):
+    def fileOpen(self, fileName):
         with open(fileName, 'r+') as rfile:
             self.codeSnip = rfile.read()
             rfile.close()
-            
+
         isBalanced = self.__checkParanthesis(self.codeSnip)
-        
+
         if(not isBalanced[0]):
             print("Error:" + isBalanced[-1])
             exit(1)
         else:
             print("Success: " + isBalanced[-1])
-            
+
         self.__searchKeywords(self.codeSnip)
-    
-    
-    def __fileWrite (self, excSnip, exc, itr):
+
+    def __fileWrite(self, excSnip, exc, itr):
         with open(f'../../test_codes/tests/X_test_{exc}_{itr}', 'w') as wfile:
             wfile.write(excSnip)
             wfile.close()
-        
-            
 
-    def  __checkParanthesis(self, codeSnip):
+    def __checkParanthesis(self, codeSnip):
         stack = []
         self.__brackets = {}
 
@@ -54,93 +52,98 @@ class ExceptionInducer:
             return (False, "Brackets UnBalanced Char:{}".format(stack.pop()[0]))
 
         return (True, "Brackets are Balanced!!")
-    
-    
+
     def __searchKeywords(self, codeSnip):
         self.__keywordsList = []
-        searchDatatype = re.finditer('(int|bool|float|double|char\b)\s',codeSnip)
+        searchDatatype = re.finditer(
+            '(int|bool|float|double|char\b)\s', codeSnip)
         # print(list(searchDatatype))
         for i in searchDatatype:
-            end = i.end();l = 0
+            end = i.end()
+            l = 0
             while codeSnip[end] not in "(;":
                 if codeSnip[end] == '{':
                     l = 1
                     break
-                end+=1
+                end += 1
             if l:
                 continue
-            self.__keywordsList.append((i.end(),end))
-            
-                                    
-    def sqrtExc(self, perFileExceptions = 10, noOfExc = 20):
-        itr = 0; edit = 0
+            self.__keywordsList.append((i.end(), end))
+
+    def sqrtExc(self, perFileExceptions=10, noOfExc=20):
+        itr = 0
         n = len(self.codeSnip)
         while noOfExc:
             code = self.codeSnip
-            tempFileExceptions =perFileExceptions
+            edit = 0
+            tempFileExceptions = perFileExceptions
             locSqrt = list(re.finditer('sqrt\(', code))
             tempSnip = code
             for i in range(len(locSqrt)):
                 sqrtPtr = locSqrt[i]
                 startPos = sqrtPtr.start()
-                endPos = self.__brackets[(startPos+4,code[startPos+4])][0]+1
-                tempSnip = tempSnip.replace(code[startPos:endPos],'sqrt({})'.format(random.randrange(-999999999,-1)))
-                tempFileExceptions -=1
-                noOfExc -=1
+                endPos = self.__brackets[(startPos+4, code[startPos+4])][0]+1
+                tempSnip = tempSnip.replace(
+                    code[startPos:endPos], 'sqrt({})'.format(random.randrange(-999999999, -1)))
+                tempFileExceptions -= 1
+                noOfExc -= 1
                 edit = 1
                 if not tempFileExceptions:
-                    itr+=1
+                    itr += 1
                     tempFileExceptions = perFileExceptions
-                    self.__fileWrite(tempSnip, "SqrtNeg",itr)
+                    self.__fileWrite(tempSnip, "SqrtNeg", itr)
                     tempSnip = code
                     edit = 0
                 if not noOfExc:
                     itr += 1
-                    self.__fileWrite(tempSnip, "SqrtNeg",itr)
+                    self.__fileWrite(tempSnip, "SqrtNeg", itr)
                     edit = 0
-                    return 
+                    return
             if edit:
-                self.__fileWrite(tempSnip, "SqrtNeg",itr)
+                itr+=1
+                self.__fileWrite(tempSnip, "SqrtNeg", itr)
 
-    def indexExc(self, perFileExceptions = 10, noOfExc = 20):
+    def indexExc(self, perFileExceptions=10, noOfExc=20):
         itr = 0
         while noOfExc:
             edit = 0
             code = self.codeSnip
             tempFileExceptions = perFileExceptions
             tempCode = code
-            arrloc = re.finditer('[a-zA-Z_][a-zA-Z0-9_]*\[',code)
-            for pos in arrloc:
-                startpos = pos.start();l = 0
-                endpos = self.__brackets[(pos.end()-1,code[pos.end()-1])][0]
-                for s,e in self.__keywordsList:
+            arrLoc = re.finditer('[a-zA-Z_][a-zA-Z0-9_]*\[', code)
+            print(self.__brackets)
+            for pos in arrLoc:
+                startpos = pos.start()
+                l = 0
+                endpos = self.__brackets[(pos.end()-1, code[pos.end()-1])][0]
+                for s, e in self.__keywordsList:
                     if startpos >= s and endpos < e:
                         l = 1
+                        break
                 if l:
                     continue
-                tempCode = tempCode.replace(code[startpos:endpos+1],f'{code[startpos:pos.end()]}{random.randint(10**9,10**11)}]')
+                tempCode = tempCode.replace(
+                    code[startpos:endpos+1], f'{code[startpos:pos.end()]}{random.randint(10**9,10**11)}]')
                 edit = 1
                 tempFileExceptions -= 1
-                noOfExc -=1
+                noOfExc -= 1
                 if not tempFileExceptions:
-                    itr+=1
-                    self.__fileWrite(tempCode, "indexOut",itr)
+                    itr += 1
+                    self.__fileWrite(tempCode, "indexOut", itr)
                     tempFileExceptions = perFileExceptions
                     tempCode = code
                     edit = 0
                 if not noOfExc:
-                    itr+=1
+                    itr += 1
                     self.__fileWrite(tempCode, "indexOut", itr)
                     edit = 0
                     return
             if edit:
-                itr+=1
+                itr += 1
                 self.__fileWrite(tempCode, "indexOut", itr)
             break
-                            
-    
-            
-    def dividebyzero(self, perFileExceptions = 10, noOfExc = 20):
+
+    def divideByZero(self, perFileExceptions=10, noOfExc=20):
         itr = 0
         n = len(self.codeSnip)
         while noOfExc:
@@ -151,34 +154,33 @@ class ExceptionInducer:
             for pos in locCout:
                 startpos = pos.start()
                 while self.codeSnip[startpos] != '<':
-                    startpos+=1
-                startpos+=2
+                    startpos += 1
+                startpos += 2
                 endpos = startpos
                 while self.codeSnip[endpos] not in "<;":
-                    endpos+=1
-                code = code.replace(self.codeSnip[pos.start():endpos],f'{self.codeSnip[pos.start():startpos]}{self.codeSnip[startpos:endpos].strip()}/0')
-                edit =1 
-                tempFileExceptions -=1
-                noOfExc -=1
+                    endpos += 1
+                code = code.replace(self.codeSnip[pos.start(
+                ):endpos], f'{self.codeSnip[pos.start():startpos]}{self.codeSnip[startpos:endpos].strip()}/0')
+                edit = 1
+                tempFileExceptions -= 1
+                noOfExc -= 1
                 if not tempFileExceptions:
-                    itr +=1
+                    itr += 1
                     self.__fileWrite(code, "divideByZero", itr)
                     tempFileExceptions = perFileExceptions
                     code = self.codeSnip
-                    edit= 0
+                    edit = 0
                 if not noOfExc:
-                    itr +=1
+                    itr += 1
                     self.__fileWrite(code, "divideByZero", itr)
                     edit = 0
-                    return 
+                    return
             if edit:
-                itr +=1
+                itr += 1
                 self.__fileWrite(code, "divideByZero", itr)
             break
-                
-                    
 
-    def error_funcName(self, perFileExceptions = 10, noOfExc = 20):
+    def error_funcName(self, perFileExceptions=10, noOfExc=20):
         itr = 0
         while noOfExc:
             edit = 0
@@ -190,40 +192,37 @@ class ExceptionInducer:
                 endpos = pos.end() - 1
 
                 l = 0
-                for start,end in self.__keywordsList:
-                    
-                    if startpos>= start and endpos <= end:
-                            l = 1
+                for start, end in self.__keywordsList:
+
+                    if startpos >= start and endpos <= end:
+                        l = 1
                 if l:
                     continue
-                sp = startpos + random.randint(0,endpos-startpos)
+                sp = startpos + random.randint(0, endpos-startpos)
                 while self.codeSnip[endpos] != ')':
-                    endpos+=1
-                code = code.replace(self.codeSnip[startpos:endpos],f'{self.codeSnip[startpos:sp]}{self.codeSnip[sp].swapcase()}{self.codeSnip[sp+1:endpos]}')
-                edit =1 
-                tempFileExceptions -=1
-                noOfExc -=1
+                    endpos += 1
+                code = code.replace(
+                    self.codeSnip[startpos:endpos], f'{self.codeSnip[startpos:sp]}{self.codeSnip[sp].swapcase()}{self.codeSnip[sp+1:endpos]}')
+                edit = 1
+                tempFileExceptions -= 1
+                noOfExc -= 1
                 if not tempFileExceptions:
-                     itr +=1
-                     self.__fileWrite(code, "wrongFunc", itr)
-                     tempFileExceptions = perFileExceptions
-                     code = self.codeSnip
-                     edit = 0
+                    itr += 1
+                    self.__fileWrite(code, "wrongFunc", itr)
+                    tempFileExceptions = perFileExceptions
+                    code = self.codeSnip
+                    edit = 0
                 if not noOfExc:
                     itr += 1
                     self.__fileWrite(code, "wrongFunc", itr)
                     edit = 0
                     return
             if edit:
-                itr+=1
+                itr += 1
                 self.__fileWrite(code, "wrongFunc", itr)
             break
-    
-                
-        
-            
-            
+
 
 exc = ExceptionInducer()
 exc.fileOpen('../../test_codes/test_add.cpp')
-exc.error_funcName(1)
+exc.indexExc(2)
