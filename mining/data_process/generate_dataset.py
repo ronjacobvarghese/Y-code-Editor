9833f6444b4aaf35dbc065a4154028c4f50a55f1
Ron Jacob <generalsmoke23@gmail.com>
2022-11-04 15:50:55 +0530

    corrections on generate dataset

--- mining/data_process/generate_dataset.py
+++ mining/data_process/generate_dataset.py
@@ -19,39 +19,36 @@ class generate_dataset:
                 isValidCode = self.workingCodeFilter(codeData[0]+codeData[1],codeData[2])
                 if not isValidCode:
                     continue
-                if not os.path.exists("../../dataset"):
-                    os.mkdir("../../dataset")
-                if not os.path.exists("../../dataset/records"):
-                    os.mkdir("../../dataset/records")
+                if not os.path.exists("dataset"):
+                    os.mkdir("dataset")
+                if not os.path.exists("dataset/records"):
+                    os.mkdir("dataset/records")
                 isGenerated = self.correctCodeGenerator(codeData[2])
                 if isGenerated:
                     continue
-                self.createAST("../../dataset/records/"+str(self.correctCodeCounter)+"/code.cpp","../../dataset/records/"+str(self.correctCodeCounter))
+                self.createAST("dataset/records/"+str(self.correctCodeCounter)+"/code.cpp","dataset/records/"+str(self.correctCodeCounter),self.correctCodeCounter)
 
 
 
     def createSampleCsv(self):
-        noOfCodes = 200
-        with open("../../codeforces/code_snippets/code_snippets_OK.csv",'r+') as csvFile:
+        noOfCodes = 37243
+        with open("codeforces/code_snippets/code_snippets_OK.csv",'r+') as csvFile:
             csvReader = csv.reader(csvFile)
             for codeData in csvReader:
                 if noOfCodes==0:
                     return
-                with open("../../codeforces/code_snippets/sample.csv",'+a') as sampCsv:
-                        sampWriter = csv.writer(sampCsv)
-                        sampWriter.writerow(codeData)
                 noOfCodes -= 1
 
 
     def workingCodeFilter(self,codeId, codeSnip):
     
-        with open("../../codeforces/test_cases/test_cases.csv") as testCaseCsv:
+        with open("codeforces/test_cases/test_cases.csv") as testCaseCsv:
             testCaseData = csv.reader(testCaseCsv)
             for testCase in testCaseData:
                 testId = testCase[0] + testCase[1]
                 if testId == codeId:
                     test = testCase[2]
-            with open("./tests/test_code.cpp",'w+') as cFile:
+            with open("mining/data_process/tests/test_code.cpp",'w+') as cFile:
                     cFile.write(codeSnip)
                     
             data,temp = os.pipe()
@@ -82,7 +79,7 @@ class generate_dataset:
                     # print("codeNo: "+str(codeCounter))
             try:
                 try:
-                    s = subprocess.check_output("g++ tests/test_code.cpp -o ./tests/a.out; ./tests/a.out", stdin=data, shell=True,timeout=5)
+                    s = subprocess.check_output("g++ mining/data_process/tests/test_code.cpp -o mining/data_process/tests/a.out; mining/data_process/tests/a.out", stdin=data, shell=True,timeout=5)
                 except(subprocess.TimeoutExpired):
                     print("code time out")
                     self.wrongCodeCounter +=1
@@ -95,21 +92,21 @@ class generate_dataset:
                 print("not able to pass..")
 
     def correctCodeGenerator(self,codeSnip):
-        if os.path.exists("../../dataset/records/"+str(self.correctCodeCounter)):
+        if os.path.exists("dataset/records/"+str(self.correctCodeCounter)):
             print(f"FOLDER ALREADY EXISTS {self.correctCodeCounter}")
             return True
-        os.mkdir("../../dataset/records/"+str(self.correctCodeCounter))
-        cppFileName = "../../dataset/records/"+str(self.correctCodeCounter)+"/code.cpp"
+        os.mkdir("dataset/records/"+str(self.correctCodeCounter))
+        cppFileName = "dataset/records/"+str(self.correctCodeCounter)+"/code.cpp"
         with open(cppFileName,"w+") as cFile:
             cFile.write(codeSnip)  
         return False
         # opening csv file for codesnipets
         
     
-    def createAST(self,readLoc,genLoc):
+    def createAST(self,readLoc,genLoc,folderNo):
             codeFile = '#include "precompiled.h"\n'
             headerFiles = []
-            print("folder: ",self.correctCodeCounter)
+            print("folder: ",folderNo)
             with open(readLoc,'r+') as cFile:
                 code = cFile.read()
                 findHeader = re.finditer('[ ]*#\s*include\s*[\"<][^>\"]*[\">]',code)
@@ -118,15 +115,15 @@ class generate_dataset:
                     headerFiles.append(code[pos.start():pos.end()]+'\n')
                     tempCode = tempCode.replace(code[pos.start():pos.end()],"")
                 codeFile+=tempCode
-            with open("tests/precompiled.h",'w+') as pFile:
+            with open("mining/data_process/tests/precompiled.h",'w+') as pFile:
                 pFile.writelines(headerFiles)
-            s = subprocess.run("clang -x c++-header tests/precompiled.h -Xclang -emit-pch -o tests/precompiled.h.pch",shell=True)
-            with open("tests/temp.cpp",'w+') as tempFile:
+            s = subprocess.run("clang -x c++-header mining/data_process/tests/precompiled.h -Xclang -emit-pch -o mining/data_process/tests/precompiled.h.pch",shell=True)
+            with open("mining/data_process/tests/temp.cpp",'w+') as tempFile:
                 tempFile.write(codeFile)
-            s = subprocess.run(f"clang -Xclang -ast-dump=json -fsyntax-only -Wno-register -include-pch tests/precompiled.h.pch tests/temp.cpp >" + genLoc + "/code_ast.json",shell=True)
+            s = subprocess.run(f"clang -Xclang -ast-dump=json -fsyntax-only -Wno-register -include-pch mining/data_process/tests/precompiled.h.pch mining/data_process/tests/temp.cpp >" + genLoc + "/code_ast.json",shell=True)
                     
 
 d = generate_dataset()
 d.createSampleCsv()
-d.genDataset("../../codeforces/code_snippets/sample.csv")
-subprocess.run('python3 ../inducers/error_inducer.py',shell=True)
+d.genDataset("codeforces/code_snippets/code_snippets_OK.csv")
+print("Completed Extracting Entire Correct Code AST's")
